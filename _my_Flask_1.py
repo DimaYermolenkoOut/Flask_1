8c312be2e0b5e97cd8f1a6fd99680f4497e33e0e
DimaYermolenkoOut <dima.yermolenko@outlook.com>
2024-01-11 23:03:27 +0200

    start and about

--- _my_Flask_1.py
+++ _my_Flask_1.py
@@ -1,11 +1,17 @@
-import flask
 from flask import Flask
 
 app = Flask(__name__)
-debaug = True
-app.route("/")
-def hello():
-    return "Hello, World!"
+
+@app.route('/')
+def index():
+    return "Hello World"
+
+
+@app.route("/about")
+def about():
+    return "Тут про нас"
+
+
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
