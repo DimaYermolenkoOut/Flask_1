530ac88e77684280f50f29c968ae2247d86f9d39
DimaYermolenkoOut <dima.yermolenko@outlook.com>
2024-01-14 20:58:46 +0200

    import sqlalchemy, create sqlite, add 2 classes Users and Post,in Python consol db.create_all()

--- _my_Flask_1.py
+++ _my_Flask_1.py
@@ -1,6 +1,27 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///flask_1.db'
+db = SQLAlchemy(app)
+
+class Users(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(120), unique=True, nullable=False)
+    user_phone = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(80), unique=True, nullable=False)
+    text = db.Column(db.Text, nullable=False)
+
+    def __repr__(self):
+        return '<Post %r>' % self.title
 
 @app.route('/')
 def index():
